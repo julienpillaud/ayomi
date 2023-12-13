9ca8ce7805541d1dfbe75f0da23d994f8fc80238
Julien Pillaud <julien.pillaud@gmail.com>
2023-12-13 19:48:59 +0100

    typo

--- tests/test_rpn.py
+++ tests/test_rpn.py
@@ -1,33 +1,33 @@
 import pytest
 
-from ayomi.use_cases.rpi import ReversePolishNotation, ReversePolishNotationError
+from ayomi.use_cases.rpn import ReversePolishNotation, ReversePolishNotationError
 
 
 @pytest.mark.parametrize(
     "elements", [["1", "2"], ["1", "2", "+", "-"], ["1", "2", "3", "+"]]
 )
-def test_npi_wrong_input(elements: list[str]) -> None:
+def test_rpn_wrong_input(elements: list[str]) -> None:
     with pytest.raises(ReversePolishNotationError):
         ReversePolishNotation(elements).evaluate()
 
 
-def test_npi_simple_sum() -> None:
+def test_rpn_simple_sum() -> None:
     assert ReversePolishNotation(["1", "2", "+"]).evaluate() == 3
 
 
-def test_npi_simple_sub() -> None:
+def test_rpn_simple_sub() -> None:
     assert ReversePolishNotation(["1", "2", "-"]).evaluate() == -1
 
 
-def test_npi_simple_mul() -> None:
+def test_rpn_simple_mul() -> None:
     assert ReversePolishNotation(["1", "2", "*"]).evaluate() == 2
 
 
-def test_npi_simple_div() -> None:
+def test_rpn_simple_div() -> None:
     assert ReversePolishNotation(["1", "2", "/"]).evaluate() == 0.5
 
 
-def test_npi_complex_input() -> None:
+def test_rpn_complex_input() -> None:
     # ((10 * (6 / ((9 + 3) * -10))) + 17) + 5
     assert (
         ReversePolishNotation(
