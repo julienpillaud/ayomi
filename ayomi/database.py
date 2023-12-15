1f076450f59ef77ed4d90ad45bd8f465548eaded
Julien Pillaud <julien.pillaud@gmail.com>
2023-12-15 22:11:30 +0100

    Add docker

--- ayomi/database.py
+++ ayomi/database.py
@@ -1,5 +1,9 @@
-from sqlmodel import create_engine
+from sqlmodel import SQLModel, create_engine
 
 from ayomi.config import settings
 
 engine = create_engine(settings.DATABASE_URI)
+
+
+def init_db() -> None:
+    SQLModel.metadata.create_all(engine)
--- ayomi/main.py
+++ ayomi/main.py
@@ -1,7 +1,18 @@
+from collections.abc import AsyncIterator
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
+from ayomi.database import init_db
 from ayomi.routers import rpn
 
-app = FastAPI(title="Ayomi")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncIterator[None]:  # noqa
+    init_db()
+    yield
+
+
+app = FastAPI(title="Ayomi", lifespan=lifespan)
 
 app.include_router(rpn.router)
